3f039f579a707709a98f3777cac06ee513499195
rimon joseph <engrimonjoseph@gmail.com>
2023-05-02 11:48:26 +0300

    init movies api folder

---
+++ Movies.API/Data/MoviesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Movies.API.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.API.Models.Movie> Movie { get; set; }
+    }
+}
